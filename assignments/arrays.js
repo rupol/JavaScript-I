d659115f289f419e4fb41942db5b28e4ca63730a
Ruth Poliakon <r.poliakon@gmail.com>
2019-06-26 17:25:19 -0600

    function-conversion completed

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -2,6 +2,8 @@
 
 // The car dealer has all of their inventory housed in the array seen below.  Scroll down past the data to find out how you can help the car dealer.
 
+console.log("\n ********** ARRAYS **********");
+
 let inventory = [
   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
